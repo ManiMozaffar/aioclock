a40260fc12cec8efb41f84c801d7742788710e04
Amir Aref <amiraref808@gmail.com>
2024-04-11 23:43:55 +0330

    accept only positive numbers on Every() trigger

--- aioclock/triggers.py
+++ aioclock/triggers.py
@@ -1,13 +1,13 @@
 import asyncio
-import zoneinfo
 from abc import ABC, abstractmethod
 from copy import deepcopy
 from datetime import datetime, timedelta
 from typing import Literal, Union
 
+import zoneinfo
 from pydantic import BaseModel, model_validator
 
-from aioclock.types import EveryT, HourT, MinuteT, SecondT, Triggers
+from aioclock.types import EveryT, HourT, MinuteT, PositiveNumber, SecondT, Triggers
 
 
 class BaseTrigger(BaseModel, ABC):
@@ -69,11 +69,11 @@ class OnShutDown(Once):
 class Every(BaseTrigger):
     type_: Literal[Triggers.EVERY] = Triggers.EVERY
 
-    seconds: Union[int, float, None] = None
-    minutes: Union[int, float, None] = None
-    hours: Union[int, float, None] = None
-    days: Union[int, float, None] = None
-    weeks: Union[int, float, None] = None
+    seconds: Union[PositiveNumber, None] = None
+    minutes: Union[PositiveNumber, None] = None
+    hours: Union[PositiveNumber, None] = None
+    days: Union[PositiveNumber, None] = None
+    weeks: Union[PositiveNumber, None] = None
 
     @model_validator(mode="after")
     def validate_time_units(self):
