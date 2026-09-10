f23c22947fbc3daa4863de72004d9bf40baf5458
Amirsoroush <amirsoroush.dev@gmail.com>
2026-09-10 15:57:49 +0300

    define `inject_with_provider` helper function

--- aioclock/provider.py
+++ aioclock/provider.py
@@ -1,11 +1,41 @@
-from functools import lru_cache
+from __future__ import annotations
 
-from fast_depends import Provider
+import sys
+from functools import lru_cache
+from inspect import signature
+from typing import Any, Callable, TypeVar
+
+from fast_depends import Provider, inject
+
+if sys.version_info < (3, 10):
+    from typing_extensions import ParamSpec
+else:
+    from typing import ParamSpec
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
 
 
 @lru_cache
-def get_provider():
+def get_provider() -> Provider:
     """Return a Provider instance, which is singleton.
     This singleton is used to inject dependencies in tasks.
     """
     return Provider()
+
+
+def inject_with_provider(func: Callable[P, T]) -> Callable[P, T]:
+    """Inject dependencies using AioClock's provider across FastDepends different versions."""
+
+    # v2 -> dependency_overrides_provider
+    # v3 -> dependency_provider
+    # commit: 4beac5f4c1959f53a6bf98e42375da4b24e4ac92
+
+    provider_keyword = (
+        "dependency_provider"
+        if "dependency_provider" in signature(inject).parameters
+        else "dependency_overrides_provider"
+    )
+    inject_kwargs: dict[str, Any] = {provider_keyword: get_provider()}
+    return inject(func, **inject_kwargs)
